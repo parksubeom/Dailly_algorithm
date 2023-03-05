5339e3287c030c5ef6d8d0d0a078eca92340a358
Bumpist <104641096+parksubeom@users.noreply.github.com>
2023-03-06 02:25:14 +0900

    feat:PGS_0Lv_모음제거

---
+++ PGS_0Lv_모음제거.js
@@ -0,0 +1,16 @@
+/*
+영어에선 a, e, i, o, u 다섯 가지 알파벳을 모음으로 분류합니다. 문자열 my_string이 매개변수로 주어질 때
+ 모음을 제거한 문자열을 return하도록 solution 함수를 완성해주세요.
+
+입출력 예 #1
+
+"bus"에서 모음 u를 제거한 "bs"를 return합니다.
+입출력 예 #1
+
+"nice to meet you"에서 모음 i, o, e, u를 모두 제거한 "nc t mt y"를 return합니다.
+ */
+
+function solution(my_string) {
+    return my_string.replace(/[aeiou]/g, '');
+  }
+  
