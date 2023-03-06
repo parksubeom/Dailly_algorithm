c8906cd95afaf0a8b9568d375f938c782afbbbf0
Bumpist <104641096+parksubeom@users.noreply.github.com>
2023-03-06 10:04:16 +0900

    feat: PGS_0Lv_제곱수 판별하기

---
+++ PGS_0Lv_제곱수 판별하기.js
@@ -0,0 +1,23 @@
+/* 
+어떤 자연수를 제곱했을 때 나오는 정수를 제곱수라고 합니다. 정수 n이 매개변수로 주어질 때
+n이 제곱수라면 1을 아니라면 2를 return하도록 solution 함수를 완성해주세요
+
+입출력 예 #1
+
+144는 12의 제곱이므로 제곱수입니다. 따라서 1을 return합니다.
+입출력 예 #2
+
+976은 제곱수가 아닙니다. 따라서 2를 return합니다.
+*/
+function solution(n) {
+    /* let result = 0
+   for(let i = 1; i < n; i++){
+    if(i*i === n){
+        return result = 1
+    }else{
+        result = 2
+    }
+   }
+     return result*/
+     return Math.sqrt(n) % 1 === 0 ? 1 : 2
+ }
