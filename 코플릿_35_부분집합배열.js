9652848c1c0a347bff119b52768da12084dc9885
Bumpist <104641096+parksubeom@users.noreply.github.com>
2023-04-01 03:17:10 +0900

    코플릿 35번 부분배열집합

--- 코플릿_35_부분집합배열.js
+++ 코플릿_35_부분집합배열.js
@@ -1,4 +1,4 @@
-const powerSet = function (str) {
+/*const powerSet = function (str) {
   //arr[i]는 알파벳 순서로 정렬되어야 합니다 => 배열정렬(오름차순)
   //집합은 중복된 원소를 허용하지 않습니다. => 중복제거
   //부분집합은 빈 문자열을 포함합니다. => 재귀함수 첫 인자로 빈문자열 주고 result에 삽입
@@ -23,18 +23,32 @@ const powerSet = function (str) {
 };
 
 powerSet('abc')
+/*
 
 /*
+0 ""
+1 ""  1 a
+  2 b
+  3 b  3 bc
+
+
+["",c]
+*/
+
+
+
+
    const powerSet = function (str) {
     let arr = [...new Set(str)].sort();
-    if (arr.length === 1) return [""].concat(arr);
+    if (arr.length === 1) return [""].concat(arr);  // => ["",'j']
     let last = arr.pop();
-    let lastArr = powerSet(arr).map((x) => x + last);
+    let lastArr = powerSet(arr).map((x) => x + last); // =>['m', 'jm'] => lastarr 값이 나왔다는건, if문 만나서 탈출한거고, if문에 들어가려면 arr = j
+    console.log(lastArr);
     return powerSet(arr).concat(lastArr).sort();
 }
 
-powerSet('abc')
+console.log(powerSet('jjump'))
 
 
- */
+ 
 //["",c,b,bc,a,ac,ab,abc]
