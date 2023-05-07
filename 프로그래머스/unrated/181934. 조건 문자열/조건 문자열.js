96449023a65e5045c02ad2b2422359a82058583b
Bumpist <sooknise@naver.com>
2023-05-07 19:40:48 +0900

    [unrated] Title: 조건 문자열, Time: 0.03 ms, Memory: 33.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,8 @@
+function solution(ineq, eq, n, m) {
+    if((ineq === '>'&& eq === '=' && n >= m) || (ineq === '<'&& eq === '=' && n <= m)){
+        return 1
+    } if((ineq === '>'&& eq === '!' && n > m) || (ineq === '<'&& eq === '!' && n < m)){
+        return 1   
+    }
+    return 0
+}
