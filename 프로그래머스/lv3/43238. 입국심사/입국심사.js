894a45224fe6f0ac37db1a2a5de7690712625669
Bumpist <sooknise@naver.com>
2023-06-12 15:12:58 +0900

    [level 3] Title: 입국심사, Time: 0.20 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.js
@@ -0,0 +1,16 @@
+function solution(n, times) {
+  //최소로 걸릴 수 있는 시간 left, 최대로 걸릴 수 있는 시간 right
+  let [left, right] = [1, Math.max(...times) * n];
+    // left 가 right를 넘어갔다는 것은 left가 n보다 크거나 같아져서 n명을 수용할 수 최소값이 되있다는 것이다.
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    //sum은 mid 시간 동안 처리 할 수 있는 사람의 수
+    const sum = times.reduce((acc, time) => acc + Math.floor(mid / time), 0);
+    if (sum < n) {
+      left = mid + 1; // mid시간동안 처리가능 한 사람의 수가 줄 서있는 사람보다 많다면 레프트값을 미드 +1로 바꿔준다.
+    } else {
+      right = mid - 1;// mid시간동안 처리가능 한 사람의 수가 줄 서있는 사람보다 적다면 라이트값을 미드 +1로 바꿔준다.
+    }
+  }
+  return left; // 레프트는 최소한의 시간 
+}
