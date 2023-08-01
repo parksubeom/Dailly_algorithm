b099eb7b3362671c148496a27e9f5a7681afd2cc
Bumpist <sooknise@naver.com>
2023-08-02 00:14:19 +0900

    [Silver IV] Title: 스택, Time: 196 ms, Memory: 12552 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.js
@@ -0,0 +1,35 @@
+function solution() {
+  /*
+          문제 : 명령어대로 처리해라
+          의사코드
+          명령대로 조건문을 줘서 처리한다.
+          조건분기마다 콘솔로그를 찍으면 시간초과가 난다.
+          답을 answer에 담아서 한번에 출력하자
+           */
+  const fs = require("fs");
+  const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+  const input = fs
+    .readFileSync(filePath)
+    .toString()
+    .replace(/\r/g, "")
+    .trim()
+    .split("\n")
+    .slice(1);
+  let result = [];
+  let answer = [];
+  for (let el of input) {
+    if (el.split(" ")[0] === "push") {
+      result.push(Number(el.split(" ")[1]));
+    } else if (el === "pop") {
+      answer.push(result.length !== 0 ? result.pop() : -1);
+    } else if (el === "size") {
+      answer.push(result.length);
+    } else if (el === "empty") {
+      answer.push(result.length !== 0 ? 0 : 1);
+    } else if (el === "top") {
+      answer.push(result.length !== 0 ? result[result.length - 1] : -1);
+    }
+  }
+  console.log(answer.join("\n"));
+}
+solution();
