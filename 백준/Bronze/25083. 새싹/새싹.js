6c280eb020b813dff36f05975bdcaffb0fc529d6
Bumpist <sooknise@naver.com>
2023-11-29 14:02:36 +0900

    [Bronze V] Title: 새싹, Time: 116 ms, Memory: 9316 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.js
@@ -0,0 +1,14 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = require("fs")
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n");
+let a = "/";
+console.log(`         ,r'"7`);
+console.log("r`-_   ,'  ,/");
+console.log(` \\. ". L_r'`);
+console.log("   `~\\/");
+console.log("      |");
+console.log("      |");
+console.log("");
