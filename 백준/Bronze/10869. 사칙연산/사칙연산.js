43079f57827760a02c054b0894aa3935467a1971
Bumpist <sooknise@naver.com>
2023-11-29 13:20:12 +0900

    [Bronze V] Title: 사칙연산, Time: 128 ms, Memory: 9600 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,11 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = require("fs").readFileSync(filePath).toString().trim().split(" ");
+/* 
+a/b
+*/
+let [a, b] = input.map(Number);
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(parseInt(a / b));
+console.log(a % b);
