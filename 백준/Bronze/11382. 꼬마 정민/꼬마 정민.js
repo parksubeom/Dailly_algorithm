60dc4cae025ff2caafef0c487ac7d04443d5b03e
Bumpist <sooknise@naver.com>
2023-11-29 13:37:40 +0900

    [Bronze V] Title: 꼬마 정민, Time: 172 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.js
@@ -0,0 +1,5 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = require("fs").readFileSync(filePath).toString().trim().split(" ");
+
+let [A, B, C] = input.map(Number);
+console.log(A + B + C);
