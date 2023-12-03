be2611c76a3392cfd4b6ea2377dd3089d07e9bc7
Bumpist <sooknise@naver.com>
2023-12-03 16:58:26 +0900

    [Bronze II] Title: 수 정렬하기, Time: 244 ms, Memory: 11888 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.js
@@ -0,0 +1,39 @@
+const readline = require("readline");
+const fs = require("fs");
+
+const rl = readline.createInterface({
+  input:
+    process.platform === "linux"
+      ? process.stdin
+      : fs.createReadStream("input.txt"),
+  output: process.stdout,
+  terminal: false,
+});
+
+let inputLines = [];
+
+rl.on("line", (line) => {
+  inputLines.push(line);
+});
+/*
+도키도키 간식드리미
+
+현재 간식받을 차례를 기록하는 변수 target을 만든다.
+스택에서 맨앞의 수 부터 빼는데, 이 맨앞의 수가 타겟이 아니라면 웨이팅 배열로 이동시킨다.
+타겟이 바뀌면, 웨이팅배열과 스택배열에 다음타겟이 있는지 확인한다.
+
+
+*/
+
+rl.on("close", () => {
+  const input = inputLines
+    .slice(1)
+    .map((el) => el.split(" "))
+    .flat()
+    .map((el) => +el)
+    .sort((a, b) => a - b);
+
+  for (let el of input) {
+    console.log(el);
+  }
+});
