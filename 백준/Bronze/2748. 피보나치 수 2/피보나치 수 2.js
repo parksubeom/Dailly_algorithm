80bc42d65864dae4493e3ef185870cf3dc95ac05
Bumpist <sooknise@naver.com>
2023-12-05 14:35:15 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 180 ms, Memory: 10460 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.js
@@ -0,0 +1,48 @@
+const readline = require("readline");
+const fs = require("fs");
+const { start } = require("repl");
+
+const rl = readline.createInterface({
+  input:
+    process.platform === "linux"
+      ? process.stdin
+      : fs.createReadStream("input.txt"),
+  output: process.stdout,
+  terminal: false,
+});
+
+let inputLines = [];
+
+rl.on("line", (line) => {
+  inputLines.push(line);
+});
+/*
+덩치
+
+
+키포인트
+완벽하게 본인보다 덩치가 큰 사람들의 수를 카운트한다
+*/
+
+rl.on("close", () => {
+  const input = inputLines
+    .map((el) => el.split(" "))
+    .map((el) => el.map((el) => +el))
+    .flat();
+  let i = 0;
+  let acc = BigInt(0);
+  let cur = BigInt(1);
+  let result = [];
+  while (i < input[0]) {
+    result.push(acc);
+    acc += cur;
+    result.push(cur);
+    cur += acc;
+    i++;
+  }
+  if (input[0] === 0) {
+    console.log(0);
+  } else {
+    console.log(BigInt(result[input[0]]).toString());
+  }
+});
