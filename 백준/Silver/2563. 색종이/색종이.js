c3ea7cb6794679bbdd08010745ddeede153b9d4a
Bumpist <sooknise@naver.com>
2023-12-16 23:33:53 +0900

    [Silver V] Title: 색종이, Time: 128 ms, Memory: 10100 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.js
@@ -0,0 +1,24 @@
+const input = require('fs').readFileSync(0).toString().split('\n').map(v => v.split(' ').map(Number));
+
+const [N] = input.shift();
+const arr = new Array(100).fill().map(v => new Array(100).fill(false));
+
+for (let i = 0; i < input.length; i++) {
+    let x = input[i][0];
+    let y = input[i][1];
+
+    for (let j = 0; j < 10; j++) {
+        for (let k = 0; k < 10; k++) {
+            arr[x + j][y + k] = true;
+        }
+    }
+}
+
+const result = arr.reduce((acc, cur) => {
+    for (let el of cur) {
+        if (el) acc++;
+    }
+    return acc;
+}, 0);
+
+console.log(result);
